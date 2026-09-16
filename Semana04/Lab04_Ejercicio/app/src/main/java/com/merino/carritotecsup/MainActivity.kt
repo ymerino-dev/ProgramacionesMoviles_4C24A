025e40852ad5dcf56c0284cc3cd5584de043ed58
ymerino-dev <ana.merino@tecsup.edu.pe>
2026-09-16 14:07:42 -0500

    Formulario agrega productos a lista observable

--- Semana04/Lab04_Ejercicio/app/src/main/java/com/merino/carritotecsup/MainActivity.kt
+++ Semana04/Lab04_Ejercicio/app/src/main/java/com/merino/carritotecsup/MainActivity.kt
@@ -4,13 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.merino.carritotecsup.ui.theme.Lab04CarritoTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,28 +18,11 @@ class MainActivity : ComponentActivity() {
         setContent {
             Lab04CarritoTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Box(modifier = Modifier.padding(innerPadding)) {
+                        PantallaCarrito()
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Lab04CarritoTheme {
-        Greeting("Android")
     }
 }
