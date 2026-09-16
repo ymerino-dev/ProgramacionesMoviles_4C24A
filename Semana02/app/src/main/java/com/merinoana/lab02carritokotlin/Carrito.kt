e74be07ed400e15af11d1c4d839b7957ea50679b
ymerino-dev <ana.merino@tecsup.edu.pe>
2026-09-16 01:06:00 -0500

    Agregamos 4 productos al carrito

--- Semana02/app/src/main/java/com/merinoana/lab02carritokotlin/Carrito.kt
+++ Semana02/app/src/main/java/com/merinoana/lab02carritokotlin/Carrito.kt
@@ -13,4 +13,9 @@ fun main(){
     val carrito = mutableListOf<Producto>()
 
     println("Cliente: $nombreCliente\n")
+
+    carrito.add(Producto("Laptop HP",2500.0,1))
+    carrito.add(Producto("Mouse Logitech", 45.5, 2))
+    carrito.add(Producto("Audifonos Sony", 120.0, 1))
+    carrito.add(Producto("USB Kingston 64GB", 75.0, 3))
 }
