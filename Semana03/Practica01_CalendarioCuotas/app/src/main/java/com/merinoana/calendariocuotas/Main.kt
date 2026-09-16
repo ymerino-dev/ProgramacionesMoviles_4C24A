340728c5dee17ff9cb56b5e18744b0da0a679e04
ymerino-dev <ana.merino@tecsup.edu.pe>
2026-09-16 02:15:48 -0500

    Agrego calculos de interes, montos e impresion de encabezado

--- Semana03/Practica01_CalendarioCuotas/app/src/main/java/com/merinoana/calendariocuotas/Main.kt
+++ Semana03/Practica01_CalendarioCuotas/app/src/main/java/com/merinoana/calendariocuotas/Main.kt
@@ -21,4 +21,24 @@ fun main() {
             println("Validacion: Solo se permiten 6, 12 o 24 cuotas. Intente de nuevo.")
         }
     }
+
+    // 2. Calculos matematicos
+    val montoInicial = precio * cantidad
+
+    val porcentajeInteres = when (cuotas) {
+        6 -> 0.20
+        12 -> 0.40
+        24 -> 0.60
+        else -> 0.0
+    }
+
+    val interes = montoInicial * porcentajeInteres
+    val montoAPagar = montoInicial + interes
+    val pagoMensual = montoAPagar / cuotas
+
+    // 3. Imprimir el encabezado superior tal como en la pizarra
+    println("\nPxC -> Monto Inicial: $montoInicial")
+    println("Monto a Pagar: $montoAPagar \t Interes: $interes")
+    println("Pago Mensual: $pagoMensual\n")
+
 }
