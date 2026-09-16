75187fcf1cc992c2aef6e50344cec307515046e9
ymerino-dev <ana.merino@tecsup.edu.pe>
2026-09-16 02:13:43 -0500

    Agrega validacion con ciclo while para 6, 12 o 24 cuotas

--- Semana03/Practica01_CalendarioCuotas/app/src/main/java/com/merinoana/calendariocuotas/Main.kt
+++ Semana03/Practica01_CalendarioCuotas/app/src/main/java/com/merinoana/calendariocuotas/Main.kt
@@ -1,2 +1,24 @@
 package com.merinoana.calendariocuotas
 
+fun main() {
+    println("=== SISTEMA DE PAGOS ===")
+    print("Ingrese el nombre del producto: ")
+    val producto = readln()
+
+    print("Ingrese el precio: ")
+    val precio = readln().toDouble()
+
+    print("Ingrese la cantidad: ")
+    val cantidad = readln().toInt()
+
+    // 1. Validacion estricta
+    var cuotas = 0
+    while (cuotas != 6 && cuotas != 12 && cuotas != 24) {
+        print("N-Cuotas (6, 12 o 24): ")
+        cuotas = readln().toInt()
+
+        if (cuotas != 6 && cuotas != 12 && cuotas != 24) {
+            println("Validacion: Solo se permiten 6, 12 o 24 cuotas. Intente de nuevo.")
+        }
+    }
+}
