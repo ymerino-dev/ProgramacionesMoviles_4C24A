2d8348968f23162fa4eb00c182939077b8cbf6a1
ymerino-dev <ana.merino@tecsup.edu.pe>
2026-09-16 01:12:15 -0500

    Agregamos la funciones matematicas de Calcular el Subtotal,el IGV y calcular el total, junto a la muestra de los resultados en la consola

--- Semana02/app/src/main/java/com/merinoana/lab02carritokotlin/Carrito.kt
+++ Semana02/app/src/main/java/com/merinoana/lab02carritokotlin/Carrito.kt
@@ -22,4 +22,31 @@ fun main(){
     for (producto in carrito) {
         println("Producto agregado: ${producto.nombre}")
     }
+
+    println()
+
+    val subtotal = calcularSubtotal(carrito)
+    val igv = calcularIGV(subtotal)
+    val total = calcularTotal(subtotal, igv)
+
+    println("Subtotal : S/ $subtotal")
+    println("IGV (18%): S/ $igv")
+    println("TOTAL    : S/ $total")
+
+}
+
+fun calcularSubtotal(productos: List<Producto>): Double {
+    var subtotal = 0.0
+    for (p in productos) {
+        subtotal += p.precio * p.cantidad
+    }
+    return subtotal
+}
+
+fun calcularIGV(subtotal: Double): Double {
+    return subtotal * 0.18
+}
+
+fun calcularTotal(subtotal: Double, igv: Double): Double {
+    return subtotal + igv
 }
