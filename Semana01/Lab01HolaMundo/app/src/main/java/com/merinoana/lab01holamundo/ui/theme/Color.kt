1b8eb2f02fe2b5800f9daa9153c10d2271dc8257
ymerino-dev <ana.merino@tecsup.edu.pe>
2026-09-15 23:54:18 -0500

    Modificacion en la carpeta ui.theme con nuevos colores base del tema de la aplicacion

--- Semana01/Lab01HolaMundo/app/src/main/java/com/merinoana/lab01holamundo/ui/theme/Color.kt
+++ Semana01/Lab01HolaMundo/app/src/main/java/com/merinoana/lab01holamundo/ui/theme/Color.kt
@@ -2,10 +2,10 @@ package com.merinoana.lab01holamundo.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val Purple80 = Color(0xFF81D4FA)
+val PurpleGrey80 = Color(0xFF80DEEA)
+val Pink80 = Color(0xFFA5D6A7)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Purple40 = Color(0xFF0288D1)
+val PurpleGrey40 = Color(0xFF0097A7)
+val Pink40 = Color(0xFF388E3C)
