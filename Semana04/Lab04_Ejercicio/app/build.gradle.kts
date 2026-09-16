059e8fae159305a5b92d016054aa52119660844b
ymerino-dev <ana.merino@tecsup.edu.pe>
2026-09-16 14:27:03 -0500

    Importacion de icon y corrion en su libreria para que funcione

--- Semana04/Lab04_Ejercicio/app/build.gradle.kts
+++ Semana04/Lab04_Ejercicio/app/build.gradle.kts
@@ -51,4 +51,5 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
     debugImplementation(libs.androidx.compose.ui.tooling)
+    implementation("androidx.compose.material:material-icons-extended")
 }
