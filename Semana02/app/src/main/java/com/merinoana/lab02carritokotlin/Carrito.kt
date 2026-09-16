92e5d67410db7af67cade93b2298e4befb6efbb6
ymerino-dev <ana.merino@tecsup.edu.pe>
2026-09-16 01:00:07 -0500

    agregamos las variables mutables y no mutables

--- Semana02/app/src/main/java/com/merinoana/lab02carritokotlin/Carrito.kt
+++ Semana02/app/src/main/java/com/merinoana/lab02carritokotlin/Carrito.kt
@@ -1,5 +1,9 @@
 package com.merinoana.lab02carritokotlin
-
+data class Producto(
+    val nombre: String,
+    val precio: Double,
+    var cantidad: Int
+)
 fun main(){
     println("==================================")
     println("   CARRITO DE COMPRAS - TECSUP    ")
