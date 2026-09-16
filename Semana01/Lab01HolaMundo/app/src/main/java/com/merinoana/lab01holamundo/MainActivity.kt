d544823bc9f0f2bdc0502f60b1bb091e22cd004e
ymerino-dev <ana.merino@tecsup.edu.pe>
2026-09-15 23:32:49 -0500

    Agrega saludo personalizado con mi nombre y curso en el Greeting

--- Semana01/Lab01HolaMundo/app/src/main/java/com/merinoana/lab01holamundo/MainActivity.kt
+++ Semana01/Lab01HolaMundo/app/src/main/java/com/merinoana/lab01holamundo/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -11,6 +12,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.merinoana.lab01holamundo.ui.theme.Lab01HolaMundoTheme
 
 class MainActivity : ComponentActivity() {
@@ -32,10 +34,13 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Column (modifier = modifier) {
+        Text(
+            text = "Hola, soy Ana Merino!",
+            fontSize = 24.sp
+        )
+        Text(text = "Curso: Programaciones Móviles")
+    }
 }
 
 @Preview(showBackground = true)
