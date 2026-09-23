7de5dc7bbe28f1cb30b49d2b63a4d4717a1e88c9
ymerino-dev <ana.merino@tecsup.edu.pe>
2026-09-23 07:17:25 -0500

    feat: configuracion de rutas de navegacion en Screen.kt

---
+++ Semana05/Tarea01_Navegacion6Vistas/app/src/main/java/com/merinoana/clinicasalud/screens/Screen.kt
@@ -0,0 +1,18 @@
+package com.merinoana.clinicasalud.screens
+
+sealed class Screen(val route: String) {
+    object Inicio : Screen("inicio")
+    object MisCitas : Screen("miscitas")
+    object Historial : Screen("historial")
+
+    // Rutas con paso de parámetros requeridos por la rúbrica
+    object Perfil : Screen("perfil/{medico}") {
+        fun createRoute(medico: String) = "perfil/$medico"
+    }
+    object Agendar : Screen("agendar/{medico}") {
+        fun createRoute(medico: String) = "agendar/$medico"
+    }
+    object Confirmacion : Screen("confirmacion/{medico}/{fecha}/{hora}") {
+        fun createRoute(medico: String, fecha: String, hora: String) = "confirmacion/$medico/$fecha/$hora"
+    }
+}
