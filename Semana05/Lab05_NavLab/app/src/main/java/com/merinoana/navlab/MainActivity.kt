a04efc57ae0e6faf96bc97c9161085121b9f711b
ymerino-dev <ana.merino@tecsup.edu.pe>
2026-09-22 23:52:24 -0500

    Actualiza ProfileScreen para mostrar mis datos Ana Yanira Merino Ramos

--- Semana05/Lab05_NavLab/app/src/main/java/com/merinoana/navlab/MainActivity.kt
+++ Semana05/Lab05_NavLab/app/src/main/java/com/merinoana/navlab/MainActivity.kt
@@ -14,7 +14,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            // Esto llama a tu archivo AppNavigation.kt
+           
             AppNavigation()
         }
     }
--- Semana05/Lab05_NavLab/app/src/main/java/com/merinoana/navlab/screens/ProfileScreen.kt
+++ Semana05/Lab05_NavLab/app/src/main/java/com/merinoana/navlab/screens/ProfileScreen.kt
@@ -5,8 +5,10 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.merinoana.navlab.navigation.Screen
 
 @Composable
 fun ProfileScreen(navController: NavController) {
@@ -17,11 +19,25 @@ fun ProfileScreen(navController: NavController) {
     ) {
         Text(
             text = "Mi Perfil",
-            style = MaterialTheme.typography.headlineMedium
+            style = MaterialTheme.typography.headlineMedium,
+            fontWeight = FontWeight.Bold
         )
-        Spacer(modifier = Modifier.height(16.dp))
-        Button(onClick = { navController.popBackStack() }) {
-            Text("Volver al inicio")
+        Spacer(modifier = Modifier.height(8.dp))
+
+        // ¡Aquí están tus datos modificados!
+        Text(
+            text = "Ana Yanira Merino Ramos",
+            style = MaterialTheme.typography.bodyLarge
+        )
+
+        Spacer(modifier = Modifier.height(32.dp))
+
+        Button(
+            // Regresa a Home y limpia la pila de navegación
+            onClick = { navController.navigate(Screen.Home.route) { popUpTo(0) } },
+            modifier = Modifier.fillMaxWidth(0.8f) // Hace el botón un poco más ancho, como en la imagen
+        ) {
+            Text("Ir al inicio")
         }
     }
 }
