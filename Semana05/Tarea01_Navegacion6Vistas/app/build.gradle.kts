a473a853b8edcc6065186724e34f2db347bc2047
ymerino-dev <ana.merino@tecsup.edu.pe>
2026-09-23 07:13:25 -0500

    build: agregar dependencia de navigation compose

--- Semana05/Tarea01_Navegacion6Vistas/app/build.gradle.kts
+++ Semana05/Tarea01_Navegacion6Vistas/app/build.gradle.kts
@@ -36,6 +36,8 @@ android {
 }
 
 dependencies {
+    implementation("androidx.navigation:navigation-compose:2.7.7")
+
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.activity.compose)
     implementation(libs.androidx.compose.material3)
