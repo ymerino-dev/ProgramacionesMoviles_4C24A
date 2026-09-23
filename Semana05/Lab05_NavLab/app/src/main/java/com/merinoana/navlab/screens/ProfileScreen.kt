bc4d6662e577744fcd2cf21adce531f90307f9be
ymerino-dev <ana.merino@tecsup.edu.pe>
2026-09-22 23:00:25 -0500

    Implementa ProfileScreen para mostrar la interfaz del perfil y habilitar el boton de retroceso hacia la pantalla principal

--- Semana05/Lab05_NavLab/app/src/main/java/com/merinoana/navlab/screens/ProfileScreen.kt
+++ Semana05/Lab05_NavLab/app/src/main/java/com/merinoana/navlab/screens/ProfileScreen.kt
@@ -1,2 +1,27 @@
 package com.merinoana.navlab.screens
 
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+
+@Composable
+fun ProfileScreen(navController: NavController) {
+    Column(
+        modifier = Modifier.fillMaxSize().padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Mi Perfil",
+            style = MaterialTheme.typography.headlineMedium
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = { navController.popBackStack() }) {
+            Text("Volver al inicio")
+        }
+    }
+}
